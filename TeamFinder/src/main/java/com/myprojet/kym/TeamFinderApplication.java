ed2f903ce4b8fe31ff13deed746708c91911960c
kimyoungmin <dudals5608@gmail.com>
2025-05-15 16:51:28 +0900

    Initial commit

--- TeamFinder/src/main/java/com/myprojet/kym/TeamFinderApplication.java
+++ TeamFinder/src/main/java/com/myprojet/kym/TeamFinderApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.myprojet.kym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class TeamFinderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(TeamFinderApplication.class, args);
 	}
 
 }
--- TeamFinder/src/test/java/com/myprojet/kym/TeamFinderApplicationTests.java
+++ TeamFinder/src/test/java/com/myprojet/kym/TeamFinderApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.myprojet.kym;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class TeamFinderApplicationTests {
 
 	@Test
 	void contextLoads() {
